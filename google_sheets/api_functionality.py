3bf1291ac87f6b0e2f476022b9e3c76c3bca0a21
Andrei Halici <andrei.halici@bindingpixels.com>
2022-10-31 00:10:19 -0400

    Added API functionality to retrieve historical data for stocks

--- google_sheets/api_functionality.py
+++ google_sheets/api_functionality.py
@@ -8,8 +8,6 @@ from googleapiclient.errors import HttpError
 import google.auth
 import json
 
-CSV_FILE = os.getcwd() + str("/artifacts/GOOGLEFINANCE-StocksRealTime-TEMPLATE.csv")
-
 def create_spreadsheet(creds, title):
     """
     Creates the Sheet the user has access to.
@@ -54,11 +52,11 @@ def delete_spreadsheet(creds, spreadsheetId):
         print(f"An error occurred: {error}")
         return error
 
-def update_from_csv(creds, spreadsheet_id, stock_symbol: str):
+def update_from_csv(creds, spreadsheet_id, stock_symbol: str, CSV_TEMPLATE_FILE):
     try:
         service = build('sheets', 'v4', credentials=creds)
 
-        with open(CSV_FILE) as f:
+        with open(CSV_TEMPLATE_FILE) as f:
             csvContents = f.read().replace("STOCK_SYMBOL_REPLACE_ME", stock_symbol)
 
         batch_update_spreadsheet_request_body = {
@@ -80,11 +78,8 @@ def update_from_csv(creds, spreadsheet_id, stock_symbol: str):
         }
 
         request = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id,
-                                                     body=batch_update_spreadsheet_request_body)
-        response = request.execute()
-
-        # TODO: Change code below to process the `response` dict:
-        pprint(response)
+                                                     body=batch_update_spreadsheet_request_body).execute()
+        return request
 
     except HttpError as error:
         print(f"An error occurred: {error}")
@@ -105,8 +100,7 @@ def get_values(creds, spreadsheet_id, range_name):
         result = service.spreadsheets().values().get(
             spreadsheetId=spreadsheet_id, range=range_name).execute()
 
-        json_object = json.dumps(dict(result['values']), indent=4)
-        return json_object
+        return result
 
     except HttpError as error:
         print(f"An error occurred: {error}")
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import json
 import os
+from pprint import pprint
+
+import coloredlogs, logging
 
 from fastapi import FastAPI
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from starlette.responses import RedirectResponse
-
 from google_sheets import api_functionality
+
+# Use colorlogs
+coloredlogs.install()
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = [
@@ -60,18 +66,50 @@ async def docs_redirect():
     return RedirectResponse(url='/docs')
 
 @app.get("/stocks/realtime/{ticker_symbol}")
-def get_ticker_symbol_info(ticker_symbol: str):
+def get_ticker_symbol_realtime_info(ticker_symbol: str):
+    CSV_TEMPLATE_FILE = os.getcwd() + str("/artifacts/GOOGLEFINANCE-StocksRealTime-TEMPLATE.csv")
+
     try:
         spreadsheetId = api_functionality.create_spreadsheet(creds, "GOOGLEFINANCE-StocksRealTime")
+        logging.info("The generated spreadsheetId is: %s", spreadsheetId)
 
-        api_functionality.update_from_csv(creds, spreadsheetId, ticker_symbol)
+        api_functionality.update_from_csv(creds, spreadsheetId, ticker_symbol, CSV_TEMPLATE_FILE)
 
         sheet_data = api_functionality.get_values(creds, spreadsheetId, "A4:B22")
 
-        return sheet_data
+        json_object = json.dumps(dict(sheet_data['values']), indent=4)
+        return json_object
 
-    except:
-        pass
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+
+    finally:
+        api_functionality.delete_spreadsheet(creds, spreadsheetId)
+
+@app.get("/stocks/historical/{ticker_symbol}")
+def get_ticker_symbol_historical_info(ticker_symbol: str):
+    CSV_TEMPLATE_FILE = os.getcwd() + str("/artifacts/GOOGLEFINANCE-HistoricalMarketData-TEMPLATE.csv")
+
+    try:
+        spreadsheetId = api_functionality.create_spreadsheet(creds, "GOOGLEFINANCE-StocksRealTime")
+        logging.info("The generated spreadsheetId is: %s", spreadsheetId)
+
+        api_functionality.update_from_csv(creds, spreadsheetId, ticker_symbol, CSV_TEMPLATE_FILE)
+
+        sheet_data = api_functionality.get_values(creds, spreadsheetId, "Sheet1")
+
+        # Cleanup some unused keys
+        keys_to_remove = ['range', 'majorDimension']
+
+        for key in keys_to_remove:
+            if key in sheet_data:
+                del sheet_data[key]
+
+        json_object = json.dumps(sheet_data, indent=4)
+        return json_object
+
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
 
     finally:
         api_functionality.delete_spreadsheet(creds, spreadsheetId)
